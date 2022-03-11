3c1cb90d348b134b003df696aa43e3706b8617ec
Sulaiman <mamanjr04@gmail.com>
2022-03-11 15:02:52 +0700

    1_Literal

---
+++ src/main/kotlin/1_Literal.kt
@@ -0,0 +1,12 @@
+fun main(){  //deklarasi dan inisialisasi variabel-variabel
+    var intLiteral = 5
+    var doubleLiteral = .02
+    var stringLiteral = "hello"
+   var charLiteral = '1'
+   var boolLiteral = true
+//tampilan variabel menggunakan string templete
+    println("intLiteral = $intLiteral")
+    println("doubleLiteral = $doubleLiteral")
+    println("stringLiteral = $stringLiteral")
+    println("charLiteral = $charLiteral")
+    println("boolLiteral = $boolLiteral")}
---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,8 @@
+//fungsi main adalah fungsi untuk menampilkan hasil di eksekusi
+fun main(args: Array<String>) {
+    //untuk mencetak string ke layar
+    println("Hello World!")
+
+
+
+}
